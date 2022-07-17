204c422dc6caeb67ffbfcd83b7398fa6fb36a9c8
Elliot Mangini <elliot.mangini@gmail.com>
2022-07-17 13:04:43 -0400

    Got visual metronome working, tested pattern storing- play function.

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-// Master Clock!
+// Master Clock Brain!
 
-let clockPosition = '1';
+let clockPosition = 0;
 
+setInterval(tickingFunctions, 86);
 setInterval(incrementClock, 86);
 
 function incrementClock() {
-    if (clockPosition < 64) {
+    console.log(clockPosition)
+    if (clockPosition < 63) {
         clockPosition += 1;
     } else {
-        clockPosition = 1;
+        clockPosition = 0;
     }
-    // console.log(clockPosition)
 }
+
+
+function tickingFunctions() {
+    metronomeBlink();
+    playKick();
+}
+
+// Child Functions!
+
+function metronomeBlink() {
+    if (Number.isInteger(clockPosition / 4)) {
+        console.log('blink!');
+        document.getElementById('metronomeLight').style.background = 'var(--litRed)';
+    } else {
+       document.getElementById('metronomeLight').style.background = 'var(--dimRed)';
+    }
+
+}
+
+
+function playKick() {
+    if (kickPattern.includes(clockPosition)) {
+
+    }
+}
+
+
+// Musical Memory!
+
+let kickPattern = [0, 5, 9, 13, 17];
+
+
+
+// Creating Patterns
